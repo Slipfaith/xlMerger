42393664135512dabbf19a32202ceaec3d69645e
Slipfaith <slipfaith@users.noreply.github.com>
2025-08-02 19:03:05 +0300

    delete-imprt-string

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
-from utils.i18n import i18n
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
