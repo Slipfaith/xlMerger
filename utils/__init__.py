4cab61ee7d47bb87c862f2695de7f529b0c10e51
Slipfaith <slipfaith@users.noreply.github.com>
2025-08-03 16:37:13 +0300

    Ver 2.1 - en full localisation

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 """Utility helpers and application metadata."""
 
-PREVIOUS_VERSION = "1.0"
-CURRENT_VERSION = "2.0"
+PREVIOUS_VERSION = "2.0"
+CURRENT_VERSION = "2.1"
 
 __all__ = ["PREVIOUS_VERSION", "CURRENT_VERSION"]
