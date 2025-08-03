16848fcee31e404f7409bb6c45a9dd2731ec6476
Slipfaith <slipfaith@users.noreply.github.com>
2025-08-03 19:22:16 +0300

    ver2.2 - pre

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 """Utility helpers and application metadata."""
 
-PREVIOUS_VERSION = "2.0"
-CURRENT_VERSION = "2.1"
+PREVIOUS_VERSION = "2.1"
+CURRENT_VERSION = "2.2"
 
 __all__ = ["PREVIOUS_VERSION", "CURRENT_VERSION"]
