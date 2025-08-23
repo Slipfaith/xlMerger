c5931109d3ae098b106297cd04002d8719ff7d5c
Slipfaith <slipfaith@users.noreply.github.com>
2025-08-23 18:10:37 +0300

    icon set

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 from gui.main_window import MainWindow
+
+ICON_PATH = r"E:\PythonProjects\01_icos\xlM2.0.ico"
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon(ICON_PATH))   # иконка для панели задач и диалогов
+
     window = MainWindow()
+    window.setWindowIcon(QIcon(ICON_PATH))  # иконка для титульной строки окна
     window.show()
+
     sys.exit(app.exec())
